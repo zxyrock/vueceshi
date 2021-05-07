4d1ff36367cfedf221c388345242338ea1d7967a
rock <1468943810@qq.com>
2021-05-07 23:33:21 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import bus from 'vue-bus'
-
-// createApp(App).prototype.$bus = createApp(bus)
 
 createApp(App).use(store).use(router).mount('#app')
 
