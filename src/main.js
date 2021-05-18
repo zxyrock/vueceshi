6b197c7f52c2dfd5f70c7745daff437489a94527
rock <1468943810@qq.com>
2021-05-19 00:35:28 +0800

    fastclick补丁-解决移动端点击300ms的延迟

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store'
 
 import toast from './components/common/toast'
-// 安装toast插件
+import FastClick from 'fastclick'
 
 
 createApp(App).use(store).use(router).use(toast).mount('#app')
 
 
+// 解决移动端3点击00ms的延迟
+FastClick.attach(document.body)
 
